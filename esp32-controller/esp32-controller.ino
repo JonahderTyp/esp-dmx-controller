43e0413812a40072af421681422fb40ab7349646
Jonah Wille <jonah.wille10@gmail.com>
2021-08-04 11:03:40 +0200

    Create esp32-controller.ino

---
+++ esp32-controller/esp32-controller.ino
@@ -0,0 +1,30 @@
+//this should not be public    
+
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+void setup() {
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  Serial.begin(115200);
+
+  WiFiManager wm;
+
+  bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    res = wm.autoConnect("SetupAP"); // anonymous ap
+    // res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected");
+    }
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
